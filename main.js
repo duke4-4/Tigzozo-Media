7dc312ea75264b92c040f8035d1355f1631f88d9
Duke Chizengwe <dr.duke260@gmail.com>
2024-05-07 13:04:30 +0200

    second commit

--- main.js
+++ main.js
@@ -1,14 +1,41 @@
 const changeTheme = document.querySelector('.theme');
-const darkmode = document.querySelector('.darktheme');
+const darkMode = document.querySelector('.darktheme');
 const changeLogo = document.querySelector('.theme');
 const changeContrast = document.querySelector('.theme2')
+const themeLogoDark = document.querySelector('.bottom') ;
+const themeLogoBright = document.querySelector('.top')
+const dropDown = document.querySelector('.overlay');
+const menu = document.querySelector('.hamburger'); 
+console.log(themeLogoBright.classList, "here");
 
-changeTheme.addEventListener('click', function(){
-    darkmode.classList.toggle('black')
-    changeLogo.classList.toggle('switch')
-})
+
+let IsLight = true
+
 
 changeContrast.addEventListener('click', function(){
-    darkmode.classList.toggle('black')
-    changeLogo.classList.toggle('switch')
+    
+    darkMode.classList.toggle('black')
+        if(IsLight){
+            themeLogoBright.classList.toggle('active')
+            themeLogoDark.classList.toggle('switch')
+            themeLogoBright.classList.remove('hide')
+            themeLogoBright.classList.add('show')
+            console.log(themeLogoBright);
+            IsLight = !IsLight
+
+        }else{
+            themeLogoBright.classList.remove('active')
+            themeLogoDark.classList.add('active')
+            IsLight = !IsLight
+
+
+        }
+    
 })
+menu.addEventListener('click', function(){
+    menu.classList.toggle('open')
+})
+// changeContrast.addEventListener('click', function(){
+//     darkmode.classList.toggle('black')
+//     changeLogo.classList.toggle('switch')
+// })
